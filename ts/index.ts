f0715cea2fda144d8042cf02b1cf6ff9038b9ee2
Dima <dim.kry.lis@gmail.com>
2020-03-02 12:15:52 +0200

    tests

--- ts/index.ts
+++ ts/index.ts
@@ -5,11 +5,6 @@ let home = new Home('home')
 home.addDevice(new Oven('bosch', {min: 10, max: 240, current: 20, step: 10}, {modes: ['mode1', 'mode2'], current: 0}));
 home.addDevice(new WashingMachine('WM', {min: 20, max: 100, current: 10, step: 20}, {modes: ['mode3', 'mode4'], current: 0}))
 
-
-const o = new Oven('OVEN', {min: 10, max: 240, current: 20, step: 10}, {modes: ['mode1', 'mode2'], current: 0});
-
-
-
 // home.deviceOn('bosch', 2000, () => {
 //     home.deviceOff('bosch', 2000, () => {
 //         home.deviceOn('bosch', 2000, () => { 
@@ -17,3 +12,18 @@ const o = new Oven('OVEN', {min: 10, max: 240, current: 20, step: 10}, {modes: [
 //         })
 //     })
 //  })
+
+const oven = new Oven('OVEN', {min: 10, max: 240, current: 20, step: 10}, {modes: ['mode1', 'mode2'], current: 0});
+
+let assert = chai.assert;
+
+assert.equal(oven.name, "OVEN");
+assert.equal(oven.state, false);
+
+oven.on();
+assert.equal(oven.state, true);
+
+oven.off();
+assert.equal(oven.state, false);
+
+console.log(oven.getMax())
